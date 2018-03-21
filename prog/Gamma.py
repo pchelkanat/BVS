86f16b361b382491d582b2ef02a951dfff94f10b
pchelkanat <pchelkanat@mail.ru>
2018-03-21 13:25:59 +0300

    update

--- prog/Gamma.py
+++ prog/Gamma.py
@@ -1,9 +1,18 @@
+import itertools as it
 
+def gengamma():
+    gamma=[]
+    return gamma
 
-def gengamma:
+def encryptGamma(text, gamma):
+    code=[]
+    for i,j in it.izip(text, it.cycle(gamma)):
+        code.append(chr(ord(i)^ord(j)))
+    return ''.join(code)
 
+def decryptGamma(code, gamma):
+    text=[]
+    for i, j in it.izip(code, it.cycle(gamma)):
+        text.append(chr(ord(i)^ord(j)))
 
-def encryptGamma:
-
-
-def encryptGamma:
+    return ''.join(text)
