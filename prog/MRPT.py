87dd00c8c757542d36ab1adc70380190982ad654
pchelkanat <pchelkanat@mail.ru>
2018-05-13 14:09:46 +0300

    update

--- prog/MRPT.py
+++ prog/MRPT.py
@@ -13,55 +13,22 @@ def checkN(n):
     return s, int(t)
 
 
-def EulerPhi(n):
-    result = 1
-    if not n % 2:
-        for x in range(3, n, 2):
-            if gcd(x, n) == 1:
-                result += 1
-    else:
-        for x in range(2, n):
-            if gcd(x, n) == 1:
-                result += 1
-    return result
-
-
-# print(EulerPhi(4),EulerPhi(2),EulerPhi(6))
-
-
-def isWitness(s, t, n):
-    num = int(EulerPhi(n) / 4)
-    for a in range(2, num + 1):
-        b = a ** t % n
-        if b == 1:
-            return True
-        elif b == 0:
-            for i in range(s):
-                if b ** (2 ** i) % n == (-1 % n):
-                    return True
-                else:
-                    return False
-        else:
-            return False
-
-
-def MillerRabin(n, r=13):
+def MillerRabin(n):
     # простые числа < 6 принимаются за составные, поэтому определим их
-    """
+
     if n < 6:
         return [False, False, True, True, False, True][n]
     elif n & 1 == 0:
         return False
-    """
 
     # число свидетелей простоты
-    countOfWitnesses = 0
+    #countOfWitnesses = 0
     s, t = checkN(n)
     # print(s, d)
-    for a in range(2, r + 2):
+    for a in range(2, n-1):
         b = (a ** t) % n
         if b == 1 or b == (n - 1):
-            countOfWitnesses += 1
+            #countOfWitnesses += 1
             continue
         for i in range(s):
             b = (b ** 2) % n
@@ -72,10 +39,10 @@ def MillerRabin(n, r=13):
                 break
         if a:
             return False
-    print(countOfWitnesses)
-    return countOfWitnesses, True # Простое
+    #print(countOfWitnesses)
+    return True # Простое
 
 
 # print(-1 % 10)
 # print(checkN(2), checkN(9))
-print(MillerRabin(149), MillerRabin(1105))
+#print(MillerRabin(149), MillerRabin(113))
